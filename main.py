e021ae9784f5cf174b749db9c496b60a3ceb9112
rp_2709 <rp2709@hotmail.com>
2024-08-27 21:19:48 +0200

    removed array of function associatedFunctions, replaced it with if statements and refactored the code to snake case only

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-def add_contact(nameToPhone):
+def add_contact(name_to_phone):
     name = input("Enter name : ")
     phone = input("Enter phone number : ")
-    nameToPhone[name] = phone
+    name_to_phone[name] = phone
 
-def query_phone(nameToPhone):
+def query_phone(name_to_phone):
     name = input("Enter name : ")
-    if name not in nameToPhone.keys():
+    if name not in name_to_phone.keys():
         print(f"{name} not found in contacts")
         return
-    print(f"{name}'s phone number is {nameToPhone[name]}")
+    print(f"{name}'s phone number is {name_to_phone[name]}")
 
-def list_contacts(nameToPhone):
-    names = sorted(nameToPhone.keys())
+def list_contacts(name_to_phone):
+    names = sorted(name_to_phone.keys())
     for name in names:
-        print(name," <--> ",nameToPhone[name])
+        print(name," <--> ", name_to_phone[name])
 
 
-validInputs = ["add","query","list","exit"]
-assiociatedActions = [add_contact,query_phone,list_contacts]
+VALID_INPUTS = ["add", "query", "list", "exit"]
 
 def getValidCommand():
-    userInput = input(f"Enter one of these commands : {validInputs}\n")
-    if userInput in validInputs:
-        return userInput
+    user_input = input(f"Enter one of these commands : {VALID_INPUTS}\n")
+    if user_input in VALID_INPUTS:
+        return user_input
     else:
         print("unknown command")
         return getValidCommand()
 
 
 def main():
-    nameToPhone = {}
+    name_to_phone = {}
     while True:
-        usrInput = getValidCommand()
-        if usrInput == "exit":
+        usr_input = getValidCommand()
+        if usr_input == "add":
+            add_contact(name_to_phone)
+        elif usr_input == "query":
+            query_phone(name_to_phone)
+        elif usr_input == "list":
+            list_contacts(name_to_phone)
+        elif usr_input == "exit":
             break
-        assiociatedActions[validInputs.index(usrInput)](nameToPhone)
+
     return 0
 
 if __name__ == "__main__":
